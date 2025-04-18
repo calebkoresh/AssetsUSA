4ec84190035ad4790919f85ab1a1eb46dfdf7198
Caleb Koresh <calebkoresh@Calebs-MacBook-Pro-2.local>
2025-04-18 18:53:52 -0400

    Improving notes aesthetic

--- AssetsUSA/accounts/forms.py
+++ AssetsUSA/accounts/forms.py
@@ -5,3 +5,10 @@ class NoteForm(forms.ModelForm):
     class Meta:
         model  = Note
         fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'placeholder': 'Write your note here...',
+                'rows': 8,
+                'cols': 40,
+            }),
+        }
