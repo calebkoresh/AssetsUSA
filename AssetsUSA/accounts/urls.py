d6ff8110c20a3f41358b10028b753227311bc1bd
Caleb Koresh <calebkoresh@Calebs-MacBook-Pro-2.local>
2025-04-18 16:49:59 -0400

    adding login functionality and styling

--- AssetsUSA/accounts/urls.py
+++ AssetsUSA/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import note_list, note_create
+from .views import note_list, note_create, signup
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/', signup, name='signup'),
     path('',    note_list,   name='note_list'),
     path('new/', note_create, name='note_create'),
 ] 
--- AssetsUSA/accounts/views.py
+++ AssetsUSA/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -22,3 +24,14 @@ def note_create(request):
     else:
         form = NoteForm()
     return render(request, 'accounts/note_form.html', {'form': form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
